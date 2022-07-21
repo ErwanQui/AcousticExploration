4212a63aec47738664df010edda27a60d902128f
bkfg12335 <erwan.quinot@gmail.com>
2022-07-21 14:37:14 +0200

    new try

--- src/clients/player/Listener.js
+++ src/clients/player/Listener.js
@@ -56,84 +56,78 @@ class Listener {
 		}
 
       this.compass;
+	    this.pointDegree;
 
-const isIOS =
-      navigator.userAgent.match(/(iPod|iPhone|iPad)/) &&
-      navigator.userAgent.match(/AppleWebKit/);
+		const isIOS =
+	      navigator.userAgent.match(/(iPod|iPhone|iPad)/) &&
+	      navigator.userAgent.match(/AppleWebKit/);
 
-    function init() {
-      // startBtn.addEventListener("click", startCompass);
-      navigator.geolocation.getCurrentPosition(locationHandler);
+	     console.log(isIOS)
 
-      if (!isIOS) {
-        window.addEventListener("deviceorientationabsolute", handler, true);
-      }
-    }
+	    // function init() {
+	      // startBtn.addEventListener("click", startCompass);
+	      navigator.geolocation.getCurrentPosition((position) => {
 
-    function startCompass() {
-      if (isIOS) {
-        DeviceOrientationEvent.requestPermission()
-          .then((response) => {
-            if (response === "granted") {
-              window.addEventListener("deviceorientation", handler, true);
-            } else {
-              alert("has to be allowed!");
-            }
-          })
-          .catch(() => alert("not supported"));
-      }
-    }
+	      const { latitude, longitude } = position.coords;
+	      var pointDegree = this.calcDegreeToPoint(latitude, longitude);
 
-    function handler(e) {
-      this.compass = e.webkitCompassHeading || Math.abs(e.alpha - 360);
-      // compassCircle.style.transform = `translate(-50%, -50%) rotate(${-compass}deg)`;
-      console.log(this.compass)
-      // ±15 degree
-      if (
-        (this.pointDegree < Math.abs(this.compass) &&
-          this.pointDegree + 15 > Math.abs(this.compass)) ||
-        this.pointDegree > Math.abs(this.compass + 15) ||
-        this.pointDegree < Math.abs(this.compass)
-      ) {
-        // myPoint.style.opacity = 0;
-      } else if (this.pointDegree) {
-        // myPoint.style.opacity = 1;
-      }
-    }
+	      if (pointDegree < 0) {
+	        pointDegree = pointDegree + 360;
+	    }
+	      });
 
-    this.pointDegree;
+	      if (!isIOS) {
+	        window.addEventListener("deviceorientationabsolute", (e) => {
 
-    function locationHandler(position) {
-      const { latitude, longitude } = position.coords;
-      var pointDegree = calcDegreeToPoint(latitude, longitude);
+		      this.compass = e.webkitCompassHeading || Math.abs(e.alpha - 360);
+		      // compassCircle.style.transform = `translate(-50%, -50%) rotate(${-compass}deg)`;
+		      console.log(this.compass)
+	  		}, true);
+	      }
+	    // }
 
-      if (pointDegree < 0) {
-        pointDegree = pointDegree + 360;
-      }
-    }
+	    function startCompass() {
+	      if (isIOS) {
+	        DeviceOrientationEvent.requestPermission()
+	          .then((response) => {
+	            if (response === "granted") {
+	              window.addEventListener("deviceorientation", handler, true);
+	            } else {
+	              alert("has to be allowed!");
+	            }
+	          })
+	          .catch(() => alert("not supported"));
+	      }
+	    }
 
-    function calcDegreeToPoint(latitude, longitude) {
-      // Qibla geolocation
-      const point = {
-        lat: 21.422487,
-        lng: 39.826206
-      };
+	    // function handler(e) {
+	    //   this.compass = e.webkitCompassHeading || Math.abs(e.alpha - 360);
+	    //   // compassCircle.style.transform = `translate(-50%, -50%) rotate(${-compass}deg)`;
+	    //   console.log(this.compass)
+	    //   // ±15 degree
+	    //   if (
+	    //     (this.pointDegree < Math.abs(this.compass) &&
+	    //       this.pointDegree + 15 > Math.abs(this.compass)) ||
+	    //     this.pointDegree > Math.abs(this.compass + 15) ||
+	    //     this.pointDegree < Math.abs(this.compass)
+	    //   ) {
+	    //     // myPoint.style.opacity = 0;
+	    //   } else if (this.pointDegree) {
+	    //     // myPoint.style.opacity = 1;
+	    //   }
+	    // }
 
-      const phiK = (point.lat * Math.PI) / 180.0;
-      const lambdaK = (point.lng * Math.PI) / 180.0;
-      const phi = (latitude * Math.PI) / 180.0;
-      const lambda = (longitude * Math.PI) / 180.0;
-      const psi =
-        (180.0 / Math.PI) *
-        Math.atan2(
-          Math.sin(lambdaK - lambda),
-          Math.cos(phi) * Math.tan(phiK) -
-            Math.sin(phi) * Math.cos(lambdaK - lambda)
-        );
-      return Math.round(psi);
-    }
 
-    init()
+	    // function locationHandler(position) {
+	    //   const { latitude, longitude } = position.coords;
+	    //   var pointDegree = this.calcDegreeToPoint(latitude, longitude);
+
+	    //   if (pointDegree < 0) {
+	    //     pointDegree = pointDegree + 360;
+	    //   }
+	    // }
+
+	    // init()
 
 
 
@@ -199,6 +193,29 @@ const isIOS =
 		this.display.appendChild(this.orientationDisplay)
 		this.display.appendChild(this.orientationDisplay2)
 	}
+
+
+	calcDegreeToPoint(latitude, longitude) {
+	      // Qibla geolocation
+	      const point = {
+	        lat: 21.422487,
+	        lng: 39.826206
+	      };
+
+	      const phiK = (point.lat * Math.PI) / 180.0;
+	      const lambdaK = (point.lng * Math.PI) / 180.0;
+	      const phi = (latitude * Math.PI) / 180.0;
+	      const lambda = (longitude * Math.PI) / 180.0;
+	      const psi =
+	        (180.0 / Math.PI) *
+	        Math.atan2(
+	          Math.sin(lambdaK - lambda),
+	          Math.cos(phi) * Math.tan(phiK) -
+	            Math.sin(phi) * Math.cos(lambdaK - lambda)
+	        );
+	      return Math.round(psi);
+	    }
+
 
 	LatLong2Meter(value) {
 		return (value * (Math.PI*6371000/180))
