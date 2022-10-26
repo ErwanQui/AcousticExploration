8d21d445262c7947f201a4429df187a26b0699bd
Erwan QUINOT <erwan.quinot@gmail.com>
2022-10-27 01:58:19 +0200

    fixed clean

--- src/clients/player/Binaural.js
+++ src/clients/player/Binaural.js
@@ -8,33 +8,22 @@ class Binaural {
 	    // Creating AudioContext
 	    this.audioContext = audioContext;		// Get audioContext
 	    this.playingSound;                    	// BufferSource's object
-	    this.audioStream = audioStream			// Get streaming plugin
-	    // this.gain;                            	// Gain's object
+	    this.audioStream = audioStream;			// Get streaming plugin
 		this.syncAudio;							// Store the syncObject for the scheduler (in "Sources.js")
 		this.sourceIndex = sourceIndex;			// Set the index of the audioSource
-		// this.initialized = false;
-		// this.previousBuffer = [];
-		// this.restBuffer = [];
-		// this.count = 0;
-		// this.arrayValue = []
-		// this.begin = true
-		// this.fileHead = []
-		// this.globalBuffer = [];
 		this.playingState = playingState; 		// Set the state of the audioSource (active or not)
 		this.audio;								// Audio buffer of the file played
 		this.initiate = true;					// Attribute to tell if it's the first audio to be played
 
 		// Create the gain
 	    this.gain = this.audioContext.createGain();
-
 	}
 
 	async start (url, value, norm) {
 
-		console.info("Starting source: " + this.sourceIndex)
+		console.info("Starting source: " + this.sourceIndex);
 
 	    // Initiate with current gain's value
-    	
     	if (this.playingState) {
     		this.gain.gain.setValueAtTime(value/norm, 0);
     	}
@@ -62,53 +51,64 @@ class Binaural {
 		}
 	}
 
-	StopAudio() {
-		this.audio.stop();
-	}
-
-
 	UpdateAudioSource(url) { // Stop the current playing to play an other source's audioBuffer
 
+		// Set the changing attribute of the audio file to "true"
 		this.changing = true;
-		var tempPlayingSound;
 
+		// Define the syncAudio attribute
    		this.syncAudio = {
 
-		    advanceTime: (currentTime, audioTime, dt) => {
+   			// Create the "advanceTime" attribute
+   			// @note: It's what called the scheduler (in "Sources.js") to play the sound
+   		    advanceTime: (currentTime, audioTime, dt) => {
 
+		    	// If it's a new audio file (=> it will not necessarily start at beginning)
 		    	if (this.changing) {
 
+		    		// Get the audio with the streaming plugin
 		    		var tempAudio = this.audioStream.createStreamSource();
 			      	tempAudio.streamId = url;
-			      	this.audioDuration = tempAudio.duration
 
+			      	// Get the duration of the audio file
+			      	this.audioDuration = tempAudio.duration;
+
+			      	// If it's not the first audio file to be played, dtop the previous one
 			        if (!this.initiate) {
-			        	this.audio.stop()
+			        	this.audio.stop();
 			        }
 			        else {
-			        	this.initiate = false
+			        	this.initiate = false;
 			        }
 
-
+			        // Set the new audio file to the corresponding audio of the 8-channels object
 			        this.audio = tempAudio;
 
+			        // Start the audio at the good moment to be synced (so not necessarily at beginning)
 				    this.audio.start(audioTime, audioTime - this.audioDuration*(Math.ceil(audioTime/this.audioDuration) - 1));
 
+				    // Connect it to others audioNodes
 		      		this.audio.connect(this.gain);
+
 		      		// console.log("AudioSources " + this.sourceIndex + " is now connected")
-				    
+
+					// Stop the audio at the good time				    
 				    this.audio.stop(this.audioDuration*Math.ceil(audioTime/this.audioDuration));
 
+				    // Reset the changing file attribute
 			        this.changing = false;
+
+			        // Return the global time of the next call for the scheduler
 			        return currentTime + this.audioDuration*Math.ceil(audioTime/this.audioDuration) - audioTime;
 			    }
 			    else {
+			    	// Get the audio with the streaming plugin
 			    	this.audio = this.audioStream.createStreamSource();
 			      	this.audio.streamId = url;
 
-		      		this.audio.connect(this.gain);
-
+			      	// Start and stop audio at the good time and connect it to others audioNodes
 			        this.audio.start(audioTime);
+			        this.audio.connect(this.gain);
 			        this.audio.stop(audioTime + this.audioDuration);
 
 			        return currentTime + this.audioDuration;
@@ -121,10 +121,9 @@ class Binaural {
 	}
 
 	// function to load samples
-	loadSample(url) {
+	loadSample(url) { // Load the samples and update the audioSource to add them to the scheduler
       	// console.log("File played: " + url)
       	this.UpdateAudioSource(url);
-
 	}
 
 	UpdateGain(value, norm) { // Update gain
@@ -133,8 +132,8 @@ class Binaural {
 	    this.gain.gain.setValueAtTime(value/norm, 0);
   	}
 
-  	GetSyncBuffer() {
-  		return(this.syncAudio)
+  	GetSyncBuffer() { // Return syncAudio
+  		return(this.syncAudio);
   	}
 }
 
--- src/clients/player/Sources.js
+++ src/clients/player/Sources.js
@@ -284,7 +284,7 @@ class Sources {
 					// Change the color of inactive sources to grey
 		          	this.sources[previousClosestSourcesId[i]].style.background = "grey";
 
-		          	console.log("Source " + previousClosestSourcesId[i] + " is no more playing")
+		          	console.log("Source " + (previousClosestSourcesId[i] + 1) + " is no more playing")
 
 		          	// Update the playing state of the corresponding audioSource to "inactive"
 			    	this.audioSources[this.audio2Source[i]].ChangePlayingState(false, this.sync.getLocalTime(this.sync.getSyncTime()));
@@ -360,7 +360,7 @@ class Sources {
 		    		break;
 	    	}
 
-	    	console.log("Source " + sources2Attribuate[i][0] + " is now playing");
+	    	console.log("Source " + (sources2Attribuate[i][0] + 1) + " is now playing");
 
 	    }
 
